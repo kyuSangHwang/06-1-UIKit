cbd1a251b59a0228fa92319b8f85c64c9f72ec39
DopeKS <kyusang.dev@gmail.com>
2024-05-21 15:41:05 +0900

    Initial commit : Clone leacture project

---
+++ Text/Text/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Text
+//
+//  Created by Jungman Bae on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
